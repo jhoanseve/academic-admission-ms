b3da651b22f93709565a398633c54270b9f7d2d9
jhoanseve <jhoanseve6@gmail.com>
2016-04-21 12:56:32 -0500

    Externalize files configuration

--- src/main/java/com/academic/cloud/admissions/AcademicAdmissionsMsApplication.java
+++ src/main/java/com/academic/cloud/admissions/AcademicAdmissionsMsApplication.java
@@ -1,14 +1,32 @@
 package com.academic.cloud.admissions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class AcademicAdmissionsMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcademicAdmissionsMsApplication.class, args);
 	}
+}
+
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
 }
